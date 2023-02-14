7ff5b53f4372d55eaa52b058df1b6bab5b4b55e4
NewHubBoy <1050478700@qq.com>
2023-02-14 23:01:02 +0800

    init

---
+++ main.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv")
+dotenv.config()
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+try {
+    console.log('start')
+    bot.launch();
+} catch (error) {
+    console.log(error)
+}
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
