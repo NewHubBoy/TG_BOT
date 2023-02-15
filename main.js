a32ee53f3b8fc2ae8e3b76ed635df159688367d1
NewHubBoy <1050478700@qq.com>
2023-02-15 16:12:57 +0800

    Revert "update"
    This reverts commit 17e62289edc480b7087ee70368f5541947fc21c5.

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const { Telegraf, Markup } = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const keyboard = Markup.inlineKeyboard([
-	// Markup.button.login("Login", "http://domain.tld/hash", {
-	// 	bot_username: "my_bot",
-	// 	request_write_access: true,
-	// }),
+	Markup.button.login("Login", "http://domain.tld/hash", {
+		bot_username: "my_bot",
+		request_write_access: true,
+	}),
 	Markup.button.url("❤️", "http://telegraf.js.org"),
 	Markup.button.callback("Delete", "delete"),
 ]);
